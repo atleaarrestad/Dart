937a70561b82613690b176a77169c8a129ad6ecc
Atle Aarrestad <atleaarrestad@gmail.com>
2023-07-07 00:37:37 +0200

    Shortcuts and winning condition

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@ let playercount = 0;
 let goal = 0;
 let rows = [];
 let players = [];
+let playersWon = [];
 
 function addPlayer() {
   let tableHeader = document.querySelector("#tableHeadRow");
@@ -16,7 +17,6 @@ function addPlayer() {
 
   playerInput.type = "text";
   playerInput.placeholder = `Player ${playercount + 1}`;
-  playerInput.classList.add("won");
 
   headerButton.textContent = "X";
   headerButton.classList.add("removePlayerButton");
@@ -55,20 +55,32 @@ function addRow() {
     let cell = createTableCellWithInput();
     cell.children[0]["data-player"] = players[i];
     cell.children[0]["data-row"] = rows.length;
+    let wonRow = hasPlayerWon(i);
+    if (wonRow) {
+      cell.children[0].classList.add("won-column-data");
+      cell.children[0].tabIndex = -1;
+    }
     row.appendChild(cell);
   }
   rows.push(row);
   body.appendChild(row);
   row.scrollIntoView();
 }
+const isNewRowNeeded = (rowIndex) => {
+  return rowIndex + 1 >= rows.length;
+};
 function createTableCellWithInput() {
   let cell = document.createElement("td");
   let input = document.createElement("input");
   input.type = "text";
   input.placeholder = "0";
   input.addEventListener("blur", (e) => {
-    validateInput(e);
-    calculateSum(getPlayerIndexFromID(e.target["data-player"]));
+    input.value = validateInputValue(input.value);
+    calculateSum(getPlayerIndexFromID(e.target["data-player"]), true);
+    removeFooterCellClass(
+      getPlayerIndexFromID(input["data-player"]),
+      "yellow-text"
+    );
   });
   input.addEventListener("focus", (e) => {
     if (isNewRowNeeded(e.target["data-row"])) {
@@ -76,6 +88,18 @@ function createTableCellWithInput() {
     }
   });
   input.addEventListener("keypress", (e) => inputEnterKeyHandler(e));
+  input.addEventListener("keyup", (e) => {
+    if (isFinite(e.key) || e.key === "Backspace") {
+      addFooterCellClass(
+        getPlayerIndexFromID(input["data-player"]),
+        "yellow-text"
+      );
+      let oldvalue = input.value;
+      input.value = validateInputValue(input.value);
+      calculateSum(getPlayerIndexFromID(e.target["data-player"]));
+      input.value = oldvalue;
+    }
+  });
   cell.append(input);
   return cell;
 }
@@ -87,33 +111,41 @@ function createTableCellFooter() {
   cell.append(input);
   return cell;
 }
-const validateInput = (e) => {
-  let value = e.target.value;
-  if (value.includes("+") || value.includes("-") || value.includes("*")) {
-    try {
-      value = eval(value);
-    } catch (error) {}
-  }
+const validateInputValue = (value) => {
+  try {
+    if (isNaN(value.charAt(value.length - 1)) && value.length > 1) {
+      value = value.substring(0, value.length - 1);
+    }
+    value = Math.floor(eval(value));
+  } catch (error) {}
+
   value = value === undefined || value === null || isNaN(value) ? 0 : value;
-  e.target.value = value;
+  return value;
 };
-const calculateSum = (playerIndex) => {
+const calculateSum = (playerIndex, countTowardsWin = false) => {
   let sum = 0;
   let body = document.querySelector("#tableBody");
+  let rowWon = false;
   for (let i = 0; i < body.rows.length; i++) {
-    let val = parseInt(body.rows[i].children[playerIndex].children[0].value);
-    sum += val === undefined || val === null || isNaN(val) ? 0 : val;
+    let input = body.rows[i].children[playerIndex].children[0];
+    let val = parseInt(input.value);
+    if (sum + val == goal) rowWon = i;
+    if (sum + val > goal) {
+      input.classList.add("red-text");
+    } else {
+      sum += val === undefined || val === null || isNaN(val) ? 0 : val;
+      input.classList.remove("red-text");
+    }
   }
   let footer = document.querySelector("#tableFooterRow");
   footer.children[playerIndex].children[0].value = `${sum} (${sum - goal})`;
+  if (sum == goal && countTowardsWin) setPlayerWon(playerIndex, rowWon);
+  return sum;
 };
 const calculateSumAllPlayers = () => {
   for (let i = 0; i < players.length; i++) {
     calculateSum(i);
   }
-};
-const isNewRowNeeded = (rowIndex) => {
-  return rowIndex + 1 >= rows.length;
 };
 function clearScores() {
   let tableBody = document.querySelector("#tableBody");
@@ -123,6 +155,7 @@ function clearScores() {
   }
 
   rows.length = 0;
+  removePlayersWon();
 
   addRow();
   calculateSumAllPlayers();
@@ -200,4 +233,78 @@ function removeCssClassToArrayElements(array, className) {
   array.forEach((element) => {
     element.classList.remove(className);
   });
+}
+function addFooterCellClass(index, className) {
+  let cell = getFooterCell(index);
+  cell.children[0].classList.add(className);
+}
+function removeFooterCellClass(index, className) {
+  let cell = getFooterCell(index);
+  cell.children[0].classList.remove(className);
+}
+
+const goalKeypressedHandler = (e) => {
+  if (e.key === "Enter") {
+    setFocusFirstColumnFirstCell();
+  }
+};
+const globalKeypressHandler = (e) => {
+  if (e.shiftKey && e.key === "+") {
+    e.preventDefault();
+    addPlayer();
+    setFocusPlayerName(players.length - 1);
+  }
+  if (e.shiftKey && e.key === "-") {
+    e.preventDefault();
+    if (players.length > 0) {
+      removeColumn(players.length - 1);
+      setFocusPlayerName(players.length - 1);
+    }
+  }
+  if (e.shiftKey && e.key === "C") {
+    e.preventDefault();
+    clearScores();
+  }
+};
+function setFocusFirstColumnFirstCell() {
+  let body = document.querySelector("#tableBody");
+  if (body.rows.length > 0) {
+    body.rows[0].children[0].children[0].focus();
+  }
+}
+function setFocusPlayerName(index) {
+  if (players.length === 0) return;
+  let tableHeader = document.querySelector("#tableHeadRow");
+  tableHeader.children[index].children[0].focus();
+}
+function setPlayerWon(playerIndex, rowIndex) {
+  if (hasPlayerWon(playerIndex)) return;
+  playersWon.push({ player: playerIndex, row: rowIndex });
+  let columns = getColumns(playerIndex);
+  columns.forEach((element) => {
+    element.children[0].tabIndex = -1;
+    element.children[0].classList.add("won-column-data");
+  });
+
+  let header = getHeaderCell(playerIndex);
+  header.children[0].classList.add("won-header");
+
+  let footer = getFooterCell(playerIndex);
+  footer.children[0].classList.add("won-footer");
+}
+function removePlayersWon() {
+  playersWon.length = 0;
+  for (let i = 0; i < players.length; i++) {
+    let header = getHeaderCell(i);
+    header.children[0].classList.remove("won-header");
+    let footer = getFooterCell(i);
+    footer.children[0].classList.remove("won-footer");
+  }
+}
+function hasPlayerWon(index) {
+  //return false if player has not won, otherwise it returns the row index the player won on
+  for (let i = 0; i < playersWon.length; i++) {
+    if (playersWon[i]["player"] == index) return playersWon[i]["row"];
+  }
+  return false;
 }
