633e0bb13bf7f53c418e61bcbcfb675e8c2c1ba4
KristofferRoenLie <kristoffer.roenlie@eye-share.no>
2023-07-25 13:20:00 +0200

    Games can now be saved.

--- client/src/pages/play/play-page.ts
+++ client/src/pages/play/play-page.ts
@@ -1,14 +1,16 @@
 import { range } from '@roenlie/mimic-core/array';
 import { EventOf } from '@roenlie/mimic-core/dom';
+import { DialogElement } from '@roenlie/mimic-elements/dialog';
 import { IconElement } from '@roenlie/mimic-elements/icon';
 import { sharedStyles } from '@roenlie/mimic-lit/styles';
 import { css, html, LitElement } from 'lit';
 import { customElement, query, state } from 'lit/decorators.js';
 
-import { Participant } from '../../app/client-db.js';
+import { Game, Participant } from '../../app/client-db.js';
+import { MimicDB } from './mimic-db.js';
 import DartScoreboardElement from './scoreboard-element.js';
 
-[ IconElement, DartScoreboardElement ];
+[ IconElement, DartScoreboardElement, DialogElement ];
 
 
 declare global {
@@ -19,6 +21,7 @@ declare global {
 @customElement('dart-play-page')
 export class DartPlayElement extends LitElement {
 
+	@state() protected gameId = crypto.randomUUID();
 	@state() protected goal = 250;
 	@query('dart-scoreboard') protected scoreboardEl: DartScoreboardElement;
 	@state() protected participants: Participant[] = [
@@ -93,25 +96,98 @@ export class DartPlayElement extends LitElement {
 		});
 	}
 
+	protected handleClickRestartGame() {
+		if (this.participants.some(par => par.score.length > 2)) {
+			const reset = confirm('Game currently in progress. Are you sure you wish to reset?');
+			if (!reset)
+				return;
+		}
+
+		this.participants.forEach(par => {
+			par.placement = 0;
+			par.score = [
+				{
+					calculation: '',
+					sum:         0,
+					total:       0,
+				},
+			];
+		});
+		this.participants = [ ...this.participants ];
+		this.gameId = crypto.randomUUID();
+
+		setTimeout(() => {
+			this.scoreboardEl.focusListField(0, 0);
+		});
+	}
+
+	protected async handleClickSaveGame() {
+		if (this.participants.every(par => par.score.length <= 2)) {
+			alert('Cannot save a game with less than 2 rounds played.');
+
+			return;
+		}
+
+		let setDialogText: (text: string) => void = () => {};
+		let closeDialog: () => void = () => {};
+
+		const dialogEl = document.createElement('mm-dialog');
+		dialogEl.createConfig(() => {
+			return {
+				text: 'Saving game progress.',
+			};
+		}).actions((dialog, state) => {
+			return {
+				editText: (text: string) => {
+					state.text = text;
+				},
+			};
+		}).template({
+			initialize: (dialog, state, actions) => {
+				setDialogText = actions.editText;
+				closeDialog = () => dialog.close();
+			},
+			render: (dialog, state) => html`
+			<div>
+				${ state.text }
+			</div>
+			`,
+			style: css`
+			.base {
+				bottom: 20vh;
+				--mm-dialog-color: black;
+				--mm-dialog-background-color: rgb(180, 204, 185);
+				--mm-dialog-border-color: rgb(67, 76, 68);
+			}
+			.dialog {
+				border-radius: 6px;
+				place-items: center;
+				width: 250px;
+			}
+			`,
+		});
+
+		this.renderRoot.append(dialogEl);
+
+		await MimicDB.connect('dart')
+			.collection(Game)
+			.put(new Game({
+				id:           this.gameId,
+				goal:         this.goal,
+				participants: this.participants,
+				datetime:     new Date(),
+			}), this.gameId);
+
+		setTimeout(() => {
+			setDialogText('Game saved successfully');
+			setTimeout(() => closeDialog(), 2000);
+		}, 500);
+	}
+
 	protected handlePageKeydown = (ev: KeyboardEvent) => {
 		if (ev.shiftKey && ev.code === 'KeyC') {
 			ev.preventDefault();
-
-			this.participants.forEach(par => {
-				par.placement = 0;
-				par.score = [
-					{
-						calculation: '',
-						sum:         0,
-						total:       0,
-					},
-				];
-			});
-			this.participants = [ ...this.participants ];
-
-			setTimeout(() => {
-				this.scoreboardEl.focusListField(0, 0);
-			});
+			this.handleClickRestartGame();
 		}
 
 		if (ev.shiftKey && (ev.key === '?' || ev.key === '+')) {
@@ -155,14 +231,34 @@ export class DartPlayElement extends LitElement {
 			</div>
 
 			<div class="actions">
-				<div>Add Player</div>
 				<button
-					class="add-player"
+					class="header-action"
+					@click=${ this.handleClickRestartGame }
+				>
+					<mm-icon
+						url="/Dart/dash-circle.svg"
+					></mm-icon>
+					<span>Reset game</span>
+				</button>
+
+				<button
+					class="header-action"
+					@click=${ this.handleClickSaveGame }
+				>
+					<mm-icon
+						url="/Dart/cloud-arrow-up.svg"
+					></mm-icon>
+					<span>Save game</span>
+				</button>
+
+				<button
+					class="header-action"
 					@click=${ this.handleClickAddPlayer }
 				>
 					<mm-icon
-						url="/Dart/plus-lg.svg"
+						url="/Dart/person-plus.svg"
 					></mm-icon>
+					<span>Add Player</span>
 				</button>
 			</div>
 		</div>
@@ -191,8 +287,7 @@ export class DartPlayElement extends LitElement {
 		}
 		.page-header {
 			display: grid;
-			grid-auto-flow: column;
-			grid-auto-columns: 1fr;
+			grid-template-columns: 100px 220px 1fr;
 			border: 2px solid rgb(0 0 0 / 70%);
 			box-shadow: 0px 0px 4px 1px rgb(0 0 0 / 50%);
 			border-radius: 4px;
@@ -207,12 +302,34 @@ export class DartPlayElement extends LitElement {
 			padding-block: 4px;
 			padding-inline: 8px;
 		}
+		.page-header .settings input {
+			width: 100%;
+		}
 		.page-header .shortcuts .group {
 			display: grid;
 			grid-template-columns: 60px max-content;
 		}
 		.page-header .actions {
-			place-items: center;
+			display: flex;
+			flex-flow: row wrap;
+			gap: 12px;
+		}
+		button.header-action {
+			display: grid;
+			grid-template-columns: max-content 1fr;
+			gap: 4px;
+			width: max-content;
+			height: max-content;
+			align-items: center;
+			background-color: rgb(160, 227, 254);
+			cursor: pointer;
+			padding: 4px;
+			border-radius: 6px;
+			border: 1px solid rgb(45, 69, 71);
+		}
+		button.header-action:focus-visible,
+		button.header-action:hover {
+			box-shadow: 0px 0px 3px black;
 		}
 		button.add-player {
 			width: max-content;
