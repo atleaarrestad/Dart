b76802c6dc57c266616b3d3dae5c59e636171754
Espen Jakobsen <espen@eye-share.no>
2024-10-23 14:21:25 +0200

    Could not have shared model due to game log

--- client/src/api/game-model.ts
+++ client/src/api/game-model.ts
@@ -10,6 +10,29 @@ export const $GameOut = z.object({
 	})),
 });
 
+
+export type $GameIn = z.infer<typeof $GameIn>;
+export const $GameIn = z.object({
+	id:        z.string(),
+	date:      z.string(),
+	goal:      z.number(),
+	playerIDs: z.array(z.string()),
+	rounds:    z.array(z.object({
+		playerScores: z.array(z.number()),
+	})),
+	playerResults: z.array(z.object({
+		id:           z.string(),
+		placement:    z.number(),
+		totalScore:   z.number(),
+		averageScore: z.number(),
+		overshoots:   z.optional(z.number()),
+		roundsPlayed: z.number(),
+		hasFinished:  z.boolean(),
+	})),
+	winners: z.array(z.any()),
+});
+
+// The existing $GameIn.playerResults object def does not match the data structure for playerResults from 'api/dartgame/add'. Hence new definition.
 export type $PlayerResults = z.infer<typeof $PlayerResults>;
 export const $PlayerResults = z.array(z.object({
 	id:           z.string(),
@@ -22,16 +45,3 @@ export const $PlayerResults = z.array(z.object({
 	mmrChange:    z.number(),
 	alias: 		  z.string(),
 }));
-
-export type $GameIn = z.infer<typeof $GameIn>;
-export const $GameIn = z.object({
-	id:        z.string(),
-	date:      z.string(),
-	goal:      z.number(),
-	playerIDs: z.array(z.string()),
-	rounds:    z.array(z.object({
-		playerScores: z.array(z.number()),
-	})),
-	playerResults: $PlayerResults,
-	winners: z.array(z.any()),
-});
